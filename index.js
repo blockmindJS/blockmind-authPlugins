315adcefdb3d7b44d8699b08c3a40d2d4477b020
mmeerrkkaa <merkkaa81@gmail.com>
2024-09-13 17:27:25 +0300

    1

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const CustomAuthPlugin = require('./src/CustomAuthPlugin');
 // Функция для загрузки плагина
 module.exports = (bot, options) => {
     const plugin = new CustomAuthPlugin(bot, options);
+
+    if (!bot.customPlugins) {
+        bot.customPlugins = {};
+    }
+    bot.customPlugins[bot.options.name] = plugin;
+
     plugin.start();
 };
